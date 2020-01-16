8e360d2f6c74a616e72cbca4083e0ba46d2737f5
Cameron Baney <cbaney@gmail.com>
2020-01-16 12:57:27 -0500

    Remove ES6 for IE11 development

--- components/FeaturedDatasets/FeaturedDatasets.vue
+++ components/FeaturedDatasets/FeaturedDatasets.vue
@@ -29,22 +29,74 @@
 
 <script>
 // @TODO remove
-const mockDatasets = () => {
-  return [...Array(10).keys()].map(i => {
-    return {
-      id: i,
-      name: '123',
-      type: `Category ${i}`,
-      image: 'https://placehold.it/252x252'
-    }
-  })
-}
+const mockDatasets = [
+  {
+    id: 0,
+    name: '123',
+    type: 'Category 0',
+    image: 'https://placehold.it/252x252'
+  },
+  {
+    id: 1,
+    name: '123',
+    type: 'Category 1',
+    image: 'https://placehold.it/252x252'
+  },
+  {
+    id: 2,
+    name: '123',
+    type: 'Category 2',
+    image: 'https://placehold.it/252x252'
+  },
+  {
+    id: 3,
+    name: '123',
+    type: 'Category 3',
+    image: 'https://placehold.it/252x252'
+  },
+  {
+    id: 4,
+    name: '123',
+    type: 'Category 4',
+    image: 'https://placehold.it/252x252'
+  },
+  {
+    id: 5,
+    name: '123',
+    type: 'Category 5',
+    image: 'https://placehold.it/252x252'
+  },
+  {
+    id: 6,
+    name: '123',
+    type: 'Category 6',
+    image: 'https://placehold.it/252x252'
+  },
+  {
+    id: 7,
+    name: '123',
+    type: 'Category 7',
+    image: 'https://placehold.it/252x252'
+  },
+  {
+    id: 8,
+    name: '123',
+    type: 'Category 8',
+    image: 'https://placehold.it/252x252'
+  },
+  {
+    id: 9,
+    name: '123',
+    type: 'Category 9',
+    image: 'https://placehold.it/252x252'
+  }
+]
 export default {
   name: 'FeaturedDatasets',
 
   data: function() {
     return {
-      datasets: mockDatasets()
+      datasets: mockDatasets
     }
   }
 }
